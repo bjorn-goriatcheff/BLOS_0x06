4fbe3bc37f72b494e5207d8b6f0e98f578713996
lawrencehly <32228282+lawrencehly@users.noreply.github.com>
2017-10-03 23:40:01 -0700

    Update proc.c

--- phase3/proc.c
+++ phase3/proc.c
@@ -22,6 +22,35 @@ void UserProc(void) {
    }
 }
 
+void CookerProc(void) {   // will be created by pressing 'c' key
+	int i;
+
+    for(;;) {
+    	Mutex(LOCK);
+        if(pies == 99) {
+        	cons_printf("+++++ Cooker %i: too many pies!\n", GetPid());
+        } else {
+            cons_printf("Cooker %i: making pie # %i...\n", GetPid(), ++pies);
+            for(i=0; i<LOOP; i++) asm("inb $0x80");      // pie-making time
+        }
+        Mutex(UNLOCK);
+    }
+}
+
+void EaterProc(void) {   // will be created by pressing 'e' key
+	int i;
+
+    for(;;) {
+    	Mutex(LOCK);
+        if(pies == 0) {
+        	cons_printf("----- Eater %i: no pie to eat!\n", GetPid());
+        } else {
+            cons_printf("Eater %i: eating pie # %i...\n", GetPid(), pies--);
+            for(i=0; i<LOOP; i++) asm("inb $0x80");
+        }
+        Mutex(UNLOCK);
+    }
+}
 
 
 
