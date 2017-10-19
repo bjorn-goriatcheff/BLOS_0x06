cdf2a858cb21279a5f9148cb25cadc04bf3dd750
lawrencehly <32228282+lawrencehly@users.noreply.github.com>
2017-10-19 11:02:04 -0700

    Add files via upload

---
+++ phase5/tools.h
@@ -0,0 +1,13 @@
+// tools.h, 159
+
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+#include "types.h" // need definition of 'q_t' below
+
+void MyBzero(char *, int);
+void EnQ(int, q_t *);
+int DeQ(q_t *);
+
+#endif
+
