699f38236d9224cf20af9ac1e51b08f32be7ec03
lawrencehly <32228282+lawrencehly@users.noreply.github.com>
2017-10-24 23:37:36 -0700

    Add files via upload

---
+++ phase6/proc.h
@@ -0,0 +1,10 @@
+// proc.h, 159
+
+#ifndef _PROC_H_
+#define _PROC_H_
+
+void SystemProc(void);      // PID 0, never preempted
+void UserProc(void);        // PID 1, 2, 3, ...
+void CookerProc(void);
+void EaterProc(void);
+#endif
